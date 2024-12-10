661bdbbcf1d5ab9208359825193457e7cf4eadac
TOGURO Mikito <mit@shalab.net>
2024-12-10 14:05:20 +0900

    リファクタリング  cplusplus で囲むようにした

--- inject-data.h
+++ inject-data.h
@@ -1,5 +1,16 @@
-﻿#if !defined( INJECT_DATA_H_HEADER_GUARD )
+﻿/**
+   グローバルな定数値
+   WMPaintNotify と通信するためのウィンドウメッセージ等 
+ */
+#pragma once 
+#if !defined( INJECT_DATA_H_HEADER_GUARD )
 #define INJECT_DATA_H_HEADER_GUARD 1
+
+/** RegisterWindowMessageW で登録するグローバルなウィンドウメッセージの文字列 */
+#define WM_INJECT_STRING (L"InjectionWindowMessage-bdf54460-b28b-44cb-baf2-4175162860e3")
+
+
+#if defined( __cplusplus ) 
 
 #include <array>
 
@@ -8,14 +19,13 @@ struct InjectThreadArgument{
   intptr_t controlWindowHandle;
 };
 
-
-#define WM_INJECT_STRING (L"InjectionWindowMessage")
-
 enum class InjectControl : WPARAM{
-  NOP,
+  NOP = 0,
   SHUTDOWN,
   WAKEUP,
   PAINT_MESSAGE
 };
+
+#endif /* defined( __cplusplus ) */
 
 #endif /* !defined( INJECT_DATA_H_HEADER_GUARD ) */
